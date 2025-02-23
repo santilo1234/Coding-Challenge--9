310e8806c4fe22c49647d7b8781345d2600f179d
santiago lopez <slopezarias@usf.edu>
2025-02-23 14:20:06 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -28,4 +28,31 @@ console.log(emp1.calculateAnnualSalary());
 // Expected output: 60000
 
 
+//Task 2
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); //Calling parent class constructor
+        this.teamSize = teamSize;
+
+    }
+
+getDetails() { //Includes the size of team
+    return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: ${this.salary}, Team Size: ${this.teamSize}`;
+}
+
+calculateBonus() { //calculates 10% of annual salary as bonus
+    return this.calculateAnnualSalary() * 0.1;
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+console.log(mgr1.calculateBonus()); 
+// Expected output: 9600
+
+
+
+
 
