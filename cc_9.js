e0677d00207c1e76e1f5cb386482bec8d73a4fa2
santiago lopez <slopezarias@usf.edu>
2025-02-23 15:25:46 -0500

    fixed task 4

--- cc_9.js
+++ cc_9.js
@@ -76,14 +76,20 @@ company.addEmployee(emp1);
 company.addEmployee(mgr1);
 company.listEmployees();
 // Expected output:
-// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
-// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+// "Employee Name: Alice Johnson, ID: 101, Department: Sales, Salary: 5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5"
 
 
 //Task 4
 
 Company.prototype.calculateTotalPayroll = function() { //Calculates total payroll of the company
-    return this.employee.reduce((total, emp) => total + emp.calculateAnnualSalary(), 0);
+    return this.employee.reduce((total, emp) => {
+        let annualSalary = emp.calculateAnnualSalary();
+        if (emp instanceof Manager) {
+            annualSalary += emp.calculateBonus();
+        }
+        return total + annualSalary;
+    }, 0);
 }
 
 console.log(company.calculateTotalPayroll()); 
